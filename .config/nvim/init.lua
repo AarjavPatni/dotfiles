254c86766605e10089929b42d9e0c4a3a5f6d1c7
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-02-27 23:15:01 -0500

    Update 2025-02-27 23:15:01

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- load last session
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		vim.defer_fn(function()
+			require("persistence").load()
+		end, 0.01)
+	end,
+})
+
 -- Keymaps
 vim.keymap.set("n", ";", ":", { noremap = true })
