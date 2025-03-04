3459a5c0d6e9ff7e94713b2f8cad262f2d282085
Aarjav Patni <aarjav@voltra.energy>
2025-03-04 17:35:23 -0500

    Update 2025-03-04 17:35:23

--- .voltra.zsh
+++ .voltra.zsh
@@ -1,1 +1,5 @@
 export AWS_PROFILE="AdministratorAccess-381492139979"
+
+# aliases
+alias hubble="kubectl exec -it -n gke-managed-dpv2-observability deployment/hubble-relay -c hubble-cli -- hubble"
+
