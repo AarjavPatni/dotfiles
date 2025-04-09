a65f439198ce7c23485a7d87e3aaf581a1c912e0
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-04-08 20:15:00 -0400

    Update 2025-04-08 20:15:00

--- dot_setup.sh
+++ dot_setup.sh
@@ -11,7 +11,7 @@ machine_config="$HOME/.${machine_alias}.zsh"
 dot_id="$HOME/.dot_id"
 
 # List and prompt for each machine-specific config file
-echo -e "\nDeleting existing machine-specific config files...\n"
+echo "\nDeleting existing machine-specific config files..."
 find "$HOME" -maxdepth 1 -type f -name ".*\.zsh" ! -name ".${machine_alias}.zsh" ! -name ".aliases.zsh" ! -name ".zshrc" | while read -r file; do
   if [[ -f "$file" ]]; then
     git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" update-index --assume-unchanged "$file"
@@ -30,19 +30,18 @@ echo "$machine_alias" >"$dot_id"
 git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" add ".${machine_alias}.zsh"
 git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" update-index --no-assume-unchanged ".${machine_alias}.zsh"
 
-echo -e "\nMachine configuration has been set up!"
+echo "\nMachine configuration has been set up!"
 echo "✅ Machine alias: $machine_alias"
 echo "✅ Configuration files created:"
-echo -e "  - Machine ID: $dot_id"
-echo -e "  - Machine-specific config: $machine_config\n"
+echo "  - Machine ID: $dot_id"
+echo "  - Machine-specific config: $machine_config\n"
 echo "Please add your machine-specific configurations to: $machine_config"
 
 # Run init-system.sh upon prompt
 read -p "Run init-system.sh? (y/N) " run_init
 if [[ "$run_init" =~ ^[Yy]$ ]]; then
-  # create a brewfile if it's not already there
   [[ ! -f "$HOME/$machine_alias.brewfile" ]] || touch "$HOME/$machine_alias.brewfile"
-  "$HOME/init-system.sh" "$machine_alias"
+  sh "$HOME/init-system.sh" "$machine_alias"
 fi
 
 # Configure git to track machine-specific brewfile
