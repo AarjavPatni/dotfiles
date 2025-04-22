96070aa187694cafc3d1ddd76743193f294faddf
Aarjav Patni <aarjav@voltra.energy>
2025-04-22 17:04:52 -0400

    Update 2025-04-22 17:04:52

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -9,32 +9,33 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD',           vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', function()
-        local params = vim.lsp.util.make_position_params()
-        vim.lsp.buf_request(0, 'textDocument/definition', params, function(_, result)
-            if result and result[1] then
-                vim.cmd('vsplit')
-                vim.lsp.buf.definition()
-            else
-                vim.lsp.buf.definition()
-            end
-        end)
-    end, bufopts)
-    vim.keymap.set('n', 'K',            vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi',           vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>',        vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa',    vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr',    vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wi',    function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
-    vim.keymap.set('n', '<space>D',     vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn',    vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca',    vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr',           vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>fo',    function() vim.lsp.buf.format { async = true } end, bufopts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', function()
+    local params = vim.lsp.util.make_position_params()
+    vim.lsp.buf_request(0, 'textDocument/definition', params, function(_, result)
+      -- if result and result[1] then
+      --     vim.cmd('vsplit')
+      --     vim.lsp.buf.definition()
+      -- else
+      --     vim.lsp.buf.definition()
+      -- end
+      vim.lsp.buf.definition()
+    end)
+  end, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wi', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>fo', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 -- BORDERS
@@ -44,8 +45,8 @@ end
 -- To instead override globally
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-      opts = opts or {}
-      opts.border = "rounded"
+  opts = opts or {}
+  opts.border = "rounded"
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
 
@@ -60,30 +61,30 @@ require("mason-lspconfig").setup()
 
 -- Setup each LSP server with the same default configuration
 require("mason-lspconfig").setup_handlers {
-    -- Default handler for all servers
-    function(server_name)
-        lspconfig[server_name].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            flags = { debounce_text_changes = 150 }
-        }
-    end,
-    
-    -- Override handler for specific servers if needed
-    ["lua_ls"] = function()
-        lspconfig.lua_ls.setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = { version = 'LuaJIT' },
-                    diagnostics = { globals = {'vim'} },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                    },
-                    telemetry = { enable = false },
-                },
-            },
-        }
-    end,
+  -- Default handler for all servers
+  function(server_name)
+    lspconfig[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      flags = { debounce_text_changes = 150 }
+    }
+  end,
+
+  -- Override handler for specific servers if needed
+  ["lua_ls"] = function()
+    lspconfig.lua_ls.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = { version = 'LuaJIT' },
+          diagnostics = { globals = { 'vim' } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = { enable = false },
+        },
+      },
+    }
+  end,
 }
