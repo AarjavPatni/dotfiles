9f0e3c32c24647c434e776086acb7e9e6d2b905d
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-03 18:45:00 -0400

    Update 2025-05-03 18:45:00

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -225,7 +225,10 @@ require('lazy').setup({
       local null_ls = require("null-ls")
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.prettier.with({ filetypes = { "markdown" } }),
+          null_ls.builtins.formatting.prettier.with({
+            filetypes = { "markdown" },
+          }),
+          null_ls.builtins.formatting.markdownlint,
           null_ls.builtins.diagnostics.markdownlint,
         },
       })
