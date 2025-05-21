7e5931398a6c3e138a3c70e346e795122352d3ae
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-21 16:15:01 -0400

    Update 2025-05-21 16:15:01

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -409,7 +409,7 @@ require('lazy').setup({
   {
     'lervag/vimtex',
     config = function()
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = 'skim'
       vim.g.tex_flavor = 'latex'
       vim.g.vimtex_quickfix_mode = 0
       vim.g.tex_conceal = 'abdmg'
